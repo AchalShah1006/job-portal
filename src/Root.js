f0a4c337f14e72b0392aac887e4405c7632f59f1
AchalShah1006 <shahachal.32@gmail.com>
2021-11-21 19:14:32 +0530

    added hashrouter for gh-pages

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Header from './components/Header';
 import App from './containers/App';
 
 function Root() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
